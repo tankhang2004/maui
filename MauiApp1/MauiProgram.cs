b2ff1a2a06b7e15c6ecfe4db6ef5270af2212de8
tankhang2004 <121081361+tankhang2004@users.noreply.github.com>
2024-09-14 15:30:56 +0700

    fourth

--- MauiApp1/MauiProgram.cs
+++ MauiApp1/MauiProgram.cs
@@ -10,7 +10,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.UseMauiCommunityToolkit()
+			// .UseMauiCommunityToolkit()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
